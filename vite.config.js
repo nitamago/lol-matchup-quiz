53f5285d2db5b0adc582db9913eccd9173b031f8
Masayuki <nitamagoex@yahoo.co.jp>
2025-08-27 20:51:00 +0900

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lol-quiz-app/',  // GitHub リポジトリ名
+  base: '/lol-matchup-quiz/',  // GitHub リポジトリ名
 })
 
