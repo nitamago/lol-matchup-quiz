63f5989c64e042ef970d67131e97320361dd280a
Masayuki <nitamagoex@yahoo.co.jp>
2025-09-02 06:06:15 +0900

    add developer x link

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,13 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      © {new Date().getFullYear()} nitamago.
+        © {new Date().getFullYear()} nitamago.
+        <p>
+            開発者: 
+            <a href="https://x.com/NitamagoVaingl1" target="_blank" rel="noopener noreferrer">
+                @NitamagoVaingl1
+            </a>
+        </p>
     </footer>
   );
 }
